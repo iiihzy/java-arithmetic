b521a5de27971933a96bc41d46d6ac42914396ca
liaoliao0513 <15573711756@139.com>
2024-05-12 19:38:11 +0800

    只出现一次数字 位运算

---
+++ leecode/src/arithmetic/week7/SingleNumber.java
@@ -0,0 +1,25 @@
+package arithmetic.week7;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        return ones;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while(sc.hasNextInt()) list.add(sc.nextInt());
+
+        System.out.println(new SingleNumber().singleNumber(list.stream().mapToInt(Integer::intValue).toArray()));
+    }
+}
