d4e2a255478f397e15bac6fe6175c6895d42643b
liaoliao0513 <15573711756@139.com>
2024-05-26 20:17:51 +0800

    找出第k大的异或坐标 位运算 前缀和

---
+++ leecode/src/arithmetic/week9/KthLargestValue.java
@@ -0,0 +1,24 @@
+package arithmetic.week9;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class KthLargestValue {
+    public int kthLargestValue(int[][] matrix, int k) {
+        int[][] pre = new int[matrix.length + 1][matrix[0].length + 1];
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                pre[i][j] = pre[i - 1][j] ^ pre[i][j - 1] ^ pre[i - 1][j - 1] ^ matrix[i - 1][j - 1];
+                list.add(pre[i][j]);
+            }
+        }
+        Collections.sort(list, new Comparator<Integer>() {
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        return list.get(k - 1);
+    }
+}
